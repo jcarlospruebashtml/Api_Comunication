bd44a20d1569befb0feeae9a168a7a8cf828e682
jcarlospruebashtml <42622703+jcarlospruebashtml@users.noreply.github.com>
2018-08-28 10:54:05 +0200

    Api comunication

---
+++ comunication.js
@@ -0,0 +1,20 @@
+
+"use strict";
+
+var zonadatos,boton,ruta,solicitud;
+
+function inicio(){
+	zonadatos=document.getElementById("zonadatos");
+	boton=document.getElementById("boton");
+	boton.addEventListener("click",lectura,false);
+}
+function lectura(){
+	ruta="texto.txt";
+	solicitud=new XMLHttpRequest().addEventListener("load",function(event){
+		zonadatos.innerHTML=event.target.responseText;},false);
+			solicitud.open("GET",ruta,true);
+			solicitud.send(null);	
+}
+
+
+window.addEventListener("load", inicio, false);
